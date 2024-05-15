3f30c31c2dc233da1a20384d64faaa664b10be16
Daniele Di Bella <daniele.dibella99@gmail.com>
2024-05-15 16:14:02 +0200

    first draft of the add function

--- connie/commands.py
+++ connie/commands.py
@@ -1,34 +1,62 @@
+import click
 import gspread
-from oauth2client.service_account import ServiceAccountCredentials
 import pandas as pd
+from oauth2client.service_account import ServiceAccountCredentials
 
-# Imposta i permessi necessari
-scope = ['https://spreadsheets.google.com/feeds',
-         'https://www.googleapis.com/auth/drive']
-
-# Inserisci il percorso al tuo file di credenziali
-cred_path = 'percorso/al/tuo/file/cred.json'
-
-# Autenticazione con il tuo file di credenziali
+# Necessary permits and access to the file
+scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+cred_path = r"C:\Users\danie\PycharmProjects\connie\connie-423215-4dfbde934222.json"
 credentials = ServiceAccountCredentials.from_json_keyfile_name(cred_path, scope)
-
-# Inizializza l'oggetto gspread con le credenziali
 gc = gspread.authorize(credentials)
-
-# ID del foglio di calcolo Google Sheet
-spreadsheet_id = 'ID_del_tuo_foglio'
-
-# Nome del foglio all'interno del foglio di calcolo
-sheet_name = 'Nome_del_tuo_foglio'
-
-# Apri il foglio di calcolo
+spreadsheet_id = "1u7A8cQg5bcJ5wD1mOqMBee_f2bpEruFX0RKACVoVwuM"
+sheet_name = "Foglio1"
 worksheet = gc.open_by_key(spreadsheet_id).worksheet(sheet_name)
-
-# Ottieni tutti i dati dal foglio di calcolo come lista di dizionari
 data = worksheet.get_all_records()
-
-# Converti la lista di dizionari in un DataFrame di Pandas
 df = pd.DataFrame(data)
 
-# Ora puoi utilizzare df come qualsiasi DataFrame di Pandas
-print(df.head())
+
+@click.command("add", help="Add a row of data to the work_net DataFrame")
+def add():
+    """"""
+    int_type = click.prompt("Which type of interaction you want to add?", type=click.Choice(["req_pos","adm"]))
+    if int_type.lower() == "adm":
+        object = click.prompt("Which is the object of the interaction?")
+    else:
+        object = ""
+    req_type = click.prompt("Are you applying for a position or asking for it?", type=click.Choice(["asking", "applying"]))
+    pos_type = click.prompt("What kind of position are you interested in?", type=click.Choice(["industry", "intern", "PhD"]))
+    deadline = ""
+    if req_type.lower() == "applying":
+        if click.confirm("There is a deadline?"):
+            deadline = click.prompt("When is the deadline", type=click.DateTime(formats=["%d-%m-%Y"]))
+    status = click.prompt("Which is the status of the interaction?", type=click.Choice(["p", "wtr", "wmr", "closed"]))
+    qual1, qual2 = "", ""
+    if click.confirm("There is a reference person?"):
+        name = click.prompt("Which is the name of the person you're interacting with?")
+        surname = click.prompt("Which is the surname of the person you're interacting with?")
+        if click.confirm("The person you're interacting with is a Prof.?"):
+            qual1 = "Prof."
+        else:
+            qual1 = ""
+        if click.confirm("The person you're interacting with is a Dr.?"):
+            qual2 = "Dr."
+        else:
+            qual2 = ""
+    else:
+        name, surname = "", ""
+    if click.confirm("There is a reference email?"):
+        email = click.prompt("Which is the email of the person you're interacting with?")
+    else:
+        email = ""
+    institution = click.prompt("Which institution/industry you're interacting with?")
+    city = click.prompt("In which city the person/institution is located?")
+    if click.confirm("Are you asking to take part to a project?"):
+        project = click.prompt("Which is the name of the project?")
+    else:
+        project = ""
+
+    new_row = {'int_type': int_type, 'req_type': req_type, "pos_type": pos_type, 'deadline': deadline,
+               "status": status, "qual1": qual1, "qual2": qual2, "name": name, "surname": surname,
+               "institution": institution, "city": city, "project": project, "object": object, "email": email}
+    new_row = pd.DataFrame(new_row)
+    df.append(new_row, ignore_index=True)
--- connie/main.py
+++ connie/main.py
@@ -1,10 +1,13 @@
 import click
+from connie.commands import add
 
 
 @click.group()
 def cli():
     pass
 
+
+cli.add_command(add)
 
 if __name__ == "__main__":
     cli()
